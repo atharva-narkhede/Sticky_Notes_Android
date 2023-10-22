b7f4038e31c4a9d2f9558d24c731e8d7fc93c453
Atharva Narkhede <atharvan12345@gmail.com>
2023-10-22 18:26:02 +0530

    added option to hide keyboard after clicking save button

--- app/src/main/java/com/atharva/stickynotes/MainActivity.java
+++ app/src/main/java/com/atharva/stickynotes/MainActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -35,6 +36,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 saveNote();
+                InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+                if (imm != null) {
+                    imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+                }
             }
         });
         LoadNotesfromPreferences();
